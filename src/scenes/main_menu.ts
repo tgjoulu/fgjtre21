a15e57236c773e09eb2db8650c10b54136101998
Pete Sevander <pete.sevander@gmail.com>
2021-01-29 22:51:57 +0200

    Quick menu with "start game"

---
+++ src/scenes/main_menu.ts
@@ -0,0 +1,25 @@
+import MainScene from './scenes/main';
+
+export default class MainScene extends Phaser.Scene {
+    startText: Phaser.GameObjects.Text;
+
+    constructor() {
+        super({ key: 'MainMenuScene' });
+    }
+
+    create() {
+        this.startText = this.add
+            .text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Start Game', {
+                color: '#000000',
+                fontSize: '32px',
+            })
+            .setOrigin(0.5, 0.5)
+            .setInteractive();
+
+        this.startText.on('pointerup', () => {
+            this.scene.start('MainScene');
+        });
+    }
+
+    update() {}
+}
