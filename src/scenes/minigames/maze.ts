cc120341a066c1bc9f74c1d351279ce9fe1328c6
Joonas Reinikka <joonas.reinikka@gmail.com>
2021-01-31 01:37:57 +0200

    Fix maze and add matter

--- src/scenes/minigames/maze.ts
+++ src/scenes/minigames/maze.ts
@@ -3,26 +3,37 @@ import { ShaderType } from '../../shaders/shader_manager';
 import MiniGameBase from './minigamebase';
 
 export default class Maze extends MiniGameBase {
+    hand: MazeHand;
     constructor() {
-        super({ key: 'maze' });
+        super({
+            key: 'maze',
+            physics: {
+                matter: {
+                    debug: true,
+                    gravity: {
+                        x: 0,
+                        y: 0,
+                    },
+                },
+            },
+        });
     }
 
     create() {
         super.create();
         //this.shaderManager.enableShader(this.cameras.main, ShaderType.WAVY);
         const bg = this.add
-            .image(
-                this.cameras.main.width / 2 + 58,
-                this.cameras.main.height / 2 + 33,
-                'minigame_1_bg'
-            )
+            .image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'minigame_1_bg')
             .setScale(4);
 
-        const hand = new MazeHand(this, 750, 450);
-        this.input.setDraggable(hand);
+        this.hand = new MazeHand(this, this.matter.world, 750, 450);
+        this.input.setDraggable(this.hand);
+
+        const stuffGroup = this.matter.world.nextGroup();
     }
 
     update(timestamp: number, dt: number) {
         super.update(timestamp, dt);
+        this.hand.update(dt);
     }
 }
