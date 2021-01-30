557c4d318869ada624b4914ab801c3ba81840cc0
Pete Sevander <pete.sevander@gmail.com>
2021-01-30 23:08:52 +0200

    Random romu spawning

---
+++ src/scenes/minigames/romukasa.ts
@@ -0,0 +1,63 @@
+import MiniGameBase from './minigamebase';
+
+let stuff = [
+    'bag_of_chips',
+    'bag',
+    'banana',
+    'can',
+    'chips',
+    'egg',
+    'empty_bag_of_chips',
+    'glove',
+    'kebab',
+    'mask',
+    'nobel',
+    'panties',
+    'phone',
+    'pika',
+    'pizza',
+    'rum',
+    'shirt',
+    'sock',
+    'wallet',
+];
+
+class Romu extends Phaser.Physics.Arcade.Sprite {
+    parent: Phaser.Scene;
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, Phaser.Math.RND.uuid());
+
+        this.parent = scene;
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.setScale(4);
+        this.setCollideWorldBounds(false);
+        this.body.setSize(32, 32);
+        this.body.setOffset(16, 16);
+        this.randomize();
+    }
+
+    public randomize() {
+        this.setTexture(Phaser.Math.RND.pick(stuff));
+        this.setRotation(Phaser.Math.RND.rotation());
+        this.setRandomPosition(this.x - 200, this.y - 150, 200, 200);
+    }
+}
+
+export default class Pile extends MiniGameBase {
+    constructor() {
+        super({ key: 'pile' });
+    }
+
+    create() {
+        super.create();
+        let centerX = this.cameras.main.width / 2;
+        let centerY = this.cameras.main.height / 2;
+        let amount = (Phaser.Math.RND.integer() % 10) + 8;
+        for (let i = 1; i <= amount; i++) {
+            new Romu(this, centerX, centerY);
+        }
+    }
+}
