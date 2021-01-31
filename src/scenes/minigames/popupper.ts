d922e774fbcc0b5a4258bc14b4e20c5840e2d02c
jlouhela <louhela.joni@gmail.com>
2021-01-31 12:29:22 +0200

    Popup logic: missing custom hit area + sprites

--- src/scenes/minigames/popupper.ts
+++ src/scenes/minigames/popupper.ts
@@ -7,7 +7,8 @@ export default class Popupper extends MiniGameBase {
     private initialAmount: number = 3;
     private border: number = 100;
     private spawnNextTimestamp: number = 0;
-    private popups: Popup[];
+    private popupCount: number = 0;
+    private maxPopupCount: number = 12;
 
     constructor() {
         super({ key: 'popupper' });
@@ -19,31 +20,45 @@ export default class Popupper extends MiniGameBase {
             .image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'minigame_1_bg')
             .setScale(4);
 
-        this.popups = [];
         for (let i = 0; i < this.initialAmount; ++i) {
-            this.spawnPopup(99999);
+            this.forceSpawnPopup();
         }
     }
 
     update(timestamp: number, dt: number) {
         super.update(timestamp, dt);
+        if (this.popupCount == 0) {
+            console.log('Popup game BEATEN');
+            this.stop();
+        }
         if (this.spawnPopup(timestamp)) {
             this.spawnNextTimestamp =
                 timestamp + Math.random() * (this.maxDelayMS - this.minDelayMS);
         }
     }
 
+    forceSpawnPopup() {
+        const randX =
+            this.bounds.x + this.border + Math.random() * (this.bounds.width - this.border * 2);
+        const randY =
+            this.bounds.y + this.border + Math.random() * (this.bounds.height - this.border * 2);
+        let popup = new Popup(this, randX, randY);
+        popup.setInteractive();
+        popup.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+            this.popupCount--;
+            popup.destroy();
+        });
+        this.popupCount++;
+    }
+
     spawnPopup(timestamp: number) {
-        if (timestamp >= this.spawnNextTimestamp) {
-            const randX =
-                this.bounds.x + this.border + Math.random() * (this.bounds.width - this.border * 2);
-            const randY =
-                this.bounds.y +
-                this.border +
-                Math.random() * (this.bounds.height - this.border * 2);
-            this.popups.push(new Popup(this, randX, randY));
-            return true;
+        if (this.popupCount == 0 || this.popupCount >= this.maxPopupCount) {
+            return false;
         }
-        return false;
+        if (timestamp < this.spawnNextTimestamp) {
+            return false;
+        }
+        this.forceSpawnPopup();
+        return true;
     }
 }
