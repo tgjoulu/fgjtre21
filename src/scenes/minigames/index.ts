ee71a1dc7d846ba4e7434af06a6f3d4b8509b3bb
Juho Myllylahti <mutjake@iki.fi>
2021-01-31 14:43:39 +0200

    phone minigame

--- src/scenes/minigames/index.ts
+++ src/scenes/minigames/index.ts
@@ -2,5 +2,6 @@ import Maze from './maze';
 import Pizza from './pizza';
 import Pile from './romukasa';
 import Popupper from './popupper';
+import Phone from './phone';
 
-export { Maze, Pizza, Pile, Popupper };
+export { Maze, Pizza, Pile, Popupper, Phone };
